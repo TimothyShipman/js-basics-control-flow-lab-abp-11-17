7df704b68d309f888c51d839cfbc0f749a51ac48
Tim Shipman <bricklayertim@yahoo.com>
2017-11-09 20:47:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,7 @@
 function scuberGreetingForFeet (ride){
   if (ride <= 400){
     return 'This one is on me!'
+  } else if (ride > 2000 && ride < 2501){
+    return 'I will gladly take your thirty bucks.'
   }
 }
